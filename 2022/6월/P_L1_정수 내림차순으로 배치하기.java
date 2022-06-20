cf6b87554183ec9d62f2ced3472bf9e75e2a4d86
kjj13 <43058016+kjj13@users.noreply.github.com>
2022-06-20 14:40:43 +0900

    day0620

---
+++ 2022/6월/P_L1_정수 내림차순으로 배치하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+
+        String num = String.valueOf(n);
+
+        char[] arr = num.toCharArray();
+
+        Arrays.sort(arr);
+
+        num = "";
+        for(int i=arr.length-1;i>=0;i--){
+            num += arr[i];
+        }
+
+        answer = Long.parseLong(num);
+
+        return answer;
+    }
+}
